9ea94bc921ab390c3a1ffd9ca7cc4e4cd01cf72c
Plamen Koynov <pl.koynov@gmail.com>
2025-06-15 19:22:20 +0300

    Make the login form reactive

--- Roomies.Web/src/app/pages/login/components/login-form/login-form.component.ts
+++ Roomies.Web/src/app/pages/login/components/login-form/login-form.component.ts
@@ -1,16 +1,52 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      // handle login
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
--- Roomies.Web/src/app/pages/login/login-page.component.ts
+++ Roomies.Web/src/app/pages/login/login-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, MatCardModule],
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {}
